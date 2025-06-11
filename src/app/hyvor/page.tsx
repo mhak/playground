bd1ceca40a85b6543e17706db6f66311853d11a8
Meng Hak <meng.hak@xcentium.com>
2025-06-11 15:53:36 -0700

    .

--- src/app/hyvor/page.tsx
+++ src/app/hyvor/page.tsx
@@ -1,10 +1,11 @@
 export default function Comment() {
   return (
     <>
+    {/* @ts-expect-error "test" */}
     <hyvor-talk-comments
       website-id="13446"
       page-id=""
-    ></hyvor-talk-comments>
+    />
     </>
   )
 }
